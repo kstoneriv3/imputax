0fad0baaa320705336677435e4352ea37b177cc5
Kei Ishikawa <k.stoneriv3@gmail.com>
2023-08-05 02:23:28 +0100

    add iterative pca imputation

--- jax_impute/pca.py
+++ jax_impute/pca.py
@@ -8,7 +8,7 @@ from jax_impute.utils import center, uncenter, validate_input
 jnp_diag = cast(Callable[[Array], Array], jnp.diag)
 
 
-def impute_by_pca(X: Array, n_components: int) -> Array:
+def impute_by_pca(X: Array, n_components: int, n_iter: int = 1) -> Array:
     """Missing value imputation by PCA.
 
     This method can be extended to fit and transform API, by using low-rank update of SVD.
@@ -17,7 +17,8 @@ def impute_by_pca(X: Array, n_components: int) -> Array:
     X, mean = center(X)
     is_nan = jnp.isnan(X)
     X = X.at[is_nan].set(0)
-    U, S, VT = jnp.linalg.svd(X, full_matrices=False)
-    X_approx = U[:, :n_components] @ jnp_diag(S[:n_components]) @ VT[:n_components, :]
-    X_imputed = jnp.where(is_nan, X_approx, X)
-    return uncenter(X_imputed, mean)
+    for i in range(n_iter):
+        U, S, VT = jnp.linalg.svd(X, full_matrices=False)
+        X_approx = U[:, :n_components] @ jnp_diag(S[:n_components]) @ VT[:n_components, :]
+        X = jnp.where(is_nan, X_approx, X)
+    return uncenter(X, mean)
